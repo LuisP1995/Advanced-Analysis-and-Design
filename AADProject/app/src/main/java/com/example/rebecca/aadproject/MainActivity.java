e98a4e6f6ca0b243d235c3e8c64072b830e8e076
Rebecca L <Rebecca L>
2016-12-12 12:56:52 +0000

    The view changes on button click

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/MainActivity.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.rebecca.aadproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Thread myThread = new Thread(); //for splash screen purposes
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(MainActivity.this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Intent newIntent = new Intent(MainActivity.this, MainScreen.class);
+        newIntent.putExtra(EXTRA_MESSAGE, "Zu Li");
+        startActivity(newIntent);
     }
 }
---
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/MainScreen.java
@@ -0,0 +1,23 @@
+package com.example.rebecca.aadproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        Bundle bundle = getIntent().getExtras();
+        String message = bundle.getString(EXTRA_MESSAGE);
+
+        TextView textView = (TextView)findViewById(R.id.messageShow);
+        textView.setText(message);
+
+    }
+}
