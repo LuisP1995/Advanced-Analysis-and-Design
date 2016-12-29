fa14cc0d85480610d4931df98f3c8d9c7631576a
Rebecca L <Rebecca L>
2016-12-29 15:07:45 +0000

    Graph View on tabs

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/ProfileScreen.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/ProfileScreen.java
@@ -2,6 +2,8 @@ package com.example.rebecca.aadproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabWidget;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -14,7 +16,28 @@ public class ProfileScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
+        TabHost host = (TabHost)findViewById(R.id.tabHost);
+        host.setup();
+
+        //Tab 1
+        TabHost.TabSpec spec = host.newTabSpec("Pairs");
+        spec.setContent(R.id.pairs);
+        spec.setIndicator("Pairs");
+        host.addTab(spec);
+
+        //Tab 2
+        spec = host.newTabSpec("Sequence");
+        spec.setContent(R.id.sequence);
+        spec.setIndicator("Sequence");
+        host.addTab(spec);
+
+        //Tab 3
+        spec = host.newTabSpec("Image");
+        spec.setContent(R.id.image);
+        spec.setIndicator("Image");
+        host.addTab(spec);
+
+        GraphView graph = (GraphView) findViewById(R.id.pairsGraph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
