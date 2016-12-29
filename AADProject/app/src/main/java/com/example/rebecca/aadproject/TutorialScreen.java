5e47d97e9d81c9914e5f862fc26fe6c8e1b85b9e
Rebecca L <Rebecca L>
2016-12-29 12:53:31 +0000

    Tutorial Screen Created

---
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/TutorialScreen.java
@@ -0,0 +1,13 @@
+package com.example.rebecca.aadproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorialScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial_screen);
+    }
+}
