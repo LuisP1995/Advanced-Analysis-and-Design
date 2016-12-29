98732e7a8722b352ef770b12598c1000ac913f1e
Rebecca L <Rebecca L>
2016-12-29 13:40:12 +0000

    Basic Button links made

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/MainScreen.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/MainScreen.java
@@ -1,10 +1,12 @@
 package com.example.rebecca.aadproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -13,11 +15,38 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
 
-        Bundle bundle = getIntent().getExtras();
-        String message = bundle.getString(EXTRA_MESSAGE);
+        SetButtonListeners();
 
-//        TextView textView = (TextView)findViewById(R.id.messageShow);
-//        textView.setText(message);
+    }
 
+    private void SetButtonListeners() {
+        Button settings_button = (Button) findViewById(R.id.settings_button);
+        settings_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent newIntent = new Intent(MainScreen.this, SettingScreen.class);
+                startActivity(newIntent);
+            }
+        });
+
+        Button profile_button = (Button) findViewById(R.id.profile_button);
+        profile_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent newIntent = new Intent(MainScreen.this, ProfileScreen.class);
+                startActivity(newIntent);
+            }
+        });
+
+        Button info_button = (Button) findViewById(R.id.info_button);
+        info_button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Toast.makeText(getApplicationContext(), "Information Here", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
