fab2bdd7bf5ed7d270b9403fe643e8daaffb8563
Rebecca L <rebeccal_18@hotmail.co.uk>
2017-01-06 15:51:05 +0000

    global font settings

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/SettingScreen.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/SettingScreen.java
@@ -1,7 +1,14 @@
 package com.example.rebecca.aadproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class SettingScreen extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class SettingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_screen);
+
+        Button button = (Button)findViewById(R.id.save_button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Spinner spinner = (Spinner) findViewById(R.id.fontSpinner);
+                String message = spinner.getSelectedItem().toString();
+                TextView textview = (TextView) findViewById(R.id.font_textview);
+                textview.setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                        getResources().getDimension(R.dimen.mediumText));
+
+            }});
     }
 }
