e397ae38e5d9243b26a35e42e85361aacba52b87
Adam <adam.cripps2013@my.ntu.ac.uk>
2017-02-02 18:58:44 +0000

    Added the average scores and games played to profile screen, refactor unit tests to make work.

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/ProfileScreenPresenter.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/ProfileScreenPresenter.java
@@ -14,20 +14,33 @@ public class ProfileScreenPresenter {
         this.ps = ps;
         pm = new ProfileModel(this.ps);
         loadProfile();
+
     }
 
-    public float[] getPairsData() {
+    public float[] getPairsScoresData() {
         return pm.getPairsScores();
     }
     public int getPairsGamesPlayed() {return pm.getPairsPlays();}
-    public float[] getSequenceData() {
+    public int getPairsAverage() {
+        return getAverageScore(pm.getPairsScores());
+    }
+
+
+    public float[] getSequenceScoresData() {
         return pm.getSequenceScores();
     }
     public int getSequenceGamesPlayed() {return pm.getSequencePlays();}
-    public float[] getImageData() {
+    public int getSequenceAverage() {
+        return getAverageScore(pm.getSequenceScores());
+    }
+
+    public float[] getImageScoresData() {
         return pm.getImageScores();
     }
     public int getImageGamesPlayed() {return pm.getImagePlays();}
+    public int getImageAverage() {
+        return getAverageScore(pm.getImageScores());
+    }
 
     public String getUserName(){
         return pm.getUserName();
@@ -40,5 +53,16 @@ public class ProfileScreenPresenter {
             return false;
         }
     }
+
+    private int getAverageScore(float [] scores) {
+        for(int i = 0; i <scores.length; i++) {
+            if(scores[i] == 0) {
+                return i == 0 ? 0 : Math.round(scores[i-1]);
+            }
+        }
+        return Math.round(scores[scores.length-1]);
+    }
+
+
 
 }
