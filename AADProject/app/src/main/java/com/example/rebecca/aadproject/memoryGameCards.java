b00c78cf7727a77f9cc7879db8c66d4376e5c1da
Luis <luis_parcon@hotmail.com>
2017-02-06 01:28:50 +0000

    Adding class for memory game, basic constructor currently implemented

---
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/memoryGameCards.java
@@ -0,0 +1,42 @@
+package com.example.rebecca.aadproject;
+
+import android.graphics.drawable.Drawable;
+import android.widget.Button;
+import android.content.Context;
+/**
+ * A custom button class in order to flip cards and compare values on the cards
+ *
+ * Created by Luis Parcon on 03/02/2017.
+ */
+
+public class memoryGameCards extends Button
+{
+    //information obtained from the pairsGame activity
+    private int row;
+    private int column;
+    private int imageID;
+
+    //some game logic
+    protected boolean isFlipped;
+    protected boolean isMatch;
+
+    //the storage for our images
+    protected Drawable frontSide;
+    protected Drawable backSide;
+
+    /*
+    constructor for custom button class
+    param:context - a context value which is the activity
+    param: row - row in the activity
+    param: column - column in the activity
+    param: imageID - the resource value for the image
+    return: none
+     */
+    public memoryGameCards(Context context, int row, int column, int imageID)
+    {
+        super(context);
+        this.row = row;
+        this.column = column;
+        this.imageID = imageID;
+    }
+}
