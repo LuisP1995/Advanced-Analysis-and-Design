099c7d151ef33baac0127d39a9c523df953be540
Rebecca L <rebeccal_18@hotmail.co.uk>
2017-02-09 13:36:35 +0000

    Sequence game classes created

---
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/SequenceController.java
@@ -0,0 +1,29 @@
+package com.example.rebecca.aadproject;
+
+import android.widget.ImageButton;
+
+/**
+ * Created by Rebecca on 09/02/2017.
+ */
+
+public class SequenceController {
+    private SequenceScreen screen;
+    //private generated sequence
+    //private score
+
+    public SequenceController(SequenceScreen sequenceScreen) {
+        screen = sequenceScreen;
+    }
+
+    public void SetupGame() {
+        //ImageButton button = (ImageButton) screen.findViewById(R.id.seqImg1);
+        //create sequence
+
+    }
+
+    public void StartGame() {
+        //play sequence
+        //store user sequence
+        //score calculate
+    }
+}
