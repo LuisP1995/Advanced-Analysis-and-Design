f6b820b2e20a838d473f0dee6a76a1575503096b
Rebecca L <rebeccal_18@hotmail.co.uk>
2017-02-11 13:47:04 +0000

    rounds for sequence game implemetned

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/SequenceController.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/SequenceController.java
@@ -1,10 +1,12 @@
 package com.example.rebecca.aadproject;
 
+import android.content.Intent;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -12,20 +14,25 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 /**
  * Created by Rebecca on 09/02/2017.
  */
 
 public class SequenceController {
+    private int _round;
     private SequenceScreen _screen;
     private List<ImageButton> _buttonList;
     private List<Integer> _userInput;
     private Integer _score;
+    private ArrayList<ImageButton> _unusedButton;
 
     public SequenceController(SequenceScreen sequenceScreen) {
         _score = 0;
         _screen = sequenceScreen;
         _userInput = new ArrayList<>();
+        _round = 1;
     }
 
     public void SetupGame() {
@@ -41,24 +48,32 @@ public class SequenceController {
 
         ImageButton sb4 = (ImageButton) _screen.findViewById(R.id.seqImg4);
         sb4.setImageResource(R.mipmap.greensqr);
+        sb4.setVisibility(View.INVISIBLE);
 
         ImageButton sb5 = (ImageButton) _screen.findViewById(R.id.seqImg5);
+        sb5.setImageResource(R.mipmap.bluetriangle);
         sb5.setVisibility(View.INVISIBLE);
+
         ImageButton sb6 = (ImageButton) _screen.findViewById(R.id.seqImg6);
+        sb6.setImageResource(R.mipmap.yellowhexagon);
         sb6.setVisibility(View.INVISIBLE);
 
+        _unusedButton = new ArrayList<>(Arrays.asList(sb4,sb5,sb6));
 
-        List<ImageButton> buttonList = new ArrayList<>(Arrays.asList(sb1,sb2,sb3,sb4));
+        List<ImageButton> buttonList = new ArrayList<>(Arrays.asList(sb1,sb2,sb3));
 
         Collections.shuffle(buttonList);
 
         _buttonList = buttonList;
+
+        SetRound();
 
         Button but = (Button) _screen.findViewById(R.id.seqStartButton);
         but.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 PlaySequence();
+                view.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -71,6 +86,21 @@ public class SequenceController {
                 }
             });
         }
+
+        for(final ImageButton button: _unusedButton){
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    _userInput.add(view.getId());
+                    CheckRoundEnd();
+                }
+            });
+        }
+    }
+
+    private void SetRound() {
+        TextView text = (TextView) _screen.findViewById(R.id.seqRound);
+        text.setText("Round: " + Integer.toString(_round));
     }
 
     public void PlaySequence() {
@@ -78,8 +108,8 @@ public class SequenceController {
 
         for (ImageButton button: _buttonList) {
             Animation newAnimation = new AlphaAnimation(1,0);
-            newAnimation.setDuration(1000);
-            newAnimation.setStartOffset(1000 *count);
+            newAnimation.setDuration(1500);
+            newAnimation.setStartOffset(1500 *count);
             button.startAnimation(newAnimation);
             count ++;
         }
@@ -87,7 +117,6 @@ public class SequenceController {
 
     public void CheckRoundEnd(){
         if (_userInput.size() == _buttonList.size()){
-        {
             for (int i =0;i<_buttonList.size();i++){
                 ImageButton button = _buttonList.get(i);
                 Integer inputId = _userInput.get(i);
@@ -95,9 +124,37 @@ public class SequenceController {
                     _score++;
                 }
             }
+            //some pause between rounds
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            Toast.makeText(_screen.getApplicationContext(), "Score: " + _score, Toast.LENGTH_SHORT).show(); // To be deleted
+            //bonus if round 5 and score 24
+            if (_round != 5) {
+                _round++;
+                nextRound();
+            }
+            else
+            {
+                Intent newIntent = new Intent(_screen, GenScoreScreen.class);
+                newIntent.putExtra(EXTRA_MESSAGE, Integer.toString(_score));
+                _screen.startActivity(newIntent);
+            }
         }
-        Toast.makeText(_screen.getApplicationContext(), "Score: " + _score, Toast.LENGTH_SHORT).show();
-            //end round
+    }
+
+    private void nextRound() {
+        _userInput = new ArrayList<>();
+        SetRound();
+        if (_unusedButton.size() != 0) {
+            ImageButton button = _unusedButton.get(0);
+            button.setVisibility(View.VISIBLE);
+            _buttonList.add(button);
+            _unusedButton.remove(button);
         }
+        Collections.shuffle(_buttonList);
+        PlaySequence();
     }
 }
