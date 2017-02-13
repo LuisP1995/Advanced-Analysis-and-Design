8996b846fd62cdff6bcf60f94debf082182fb8e2
Adam <adam.cripps2013@my.ntu.ac.uk>
2017-02-13 16:59:31 +0000

    Made the locking mech

--- AADProject/app/src/main/java/com/example/rebecca/aadproject/MainScreen.java
+++ AADProject/app/src/main/java/com/example/rebecca/aadproject/MainScreen.java
@@ -16,65 +16,18 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
-
-        SetButtonListeners();
+        MainPresenter mainPresenter = new MainPresenter(this);
     }
 
-    private void SetButtonListeners() {
-        Button settings_button = (Button) findViewById(R.id.settings_button);
-        settings_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent newIntent = new Intent(MainScreen.this, SettingScreen.class);
-                startActivity(newIntent);
-            }
-        });
-
-        Button profile_button = (Button) findViewById(R.id.profile_button);
-        profile_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent newIntent = new Intent(MainScreen.this, ProfileScreen.class);
-                startActivity(newIntent);
-            }
-        });
-
-        Button info_button = (Button) findViewById(R.id.info_button);
-        info_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Information Here", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        Button pairs_button = (Button) findViewById(R.id.pairs_button);
-        pairs_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent newIntent = new Intent(MainScreen.this, TutorialScreen.class);
-                newIntent.putExtra(EXTRA_MESSAGE, "Pairs");
-                startActivity(newIntent);
-            }
-        });
-
+    void setSequenceGameState(boolean state) {
         Button seq_button = (Button) findViewById(R.id.seq_button);
-        seq_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent newIntent = new Intent(MainScreen.this, TutorialScreen.class);
-                newIntent.putExtra(EXTRA_MESSAGE, "Seq");
-                startActivity(newIntent);
-            }
-        });
-
-        Button img_button = (Button) findViewById(R.id.img_button);
-        img_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent newIntent = new Intent(MainScreen.this, TutorialScreen.class);
-                newIntent.putExtra(EXTRA_MESSAGE, "Image");
-                startActivity(newIntent);
-            }
-        });
+        seq_button.setEnabled(state);
     }
+
+    void setImageGameState(boolean state) {
+        Button img_button = (Button) findViewById(R.id.img_button);
+        img_button.setEnabled(state);
+    }
+
+
 }
